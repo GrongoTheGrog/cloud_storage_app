bdd0ba6ef06ac7d41fa59998bba8a2986d90be0a
Bruno Hugo <brunohugo2007@gmail.com>
2025-07-11 09:07:17 -0300

    Continued the folder service and created an item service for general purpose methods.

---
+++ src/main/java/com/grongo/cloud_storage_app/exceptions/storageExceptions/ItemNotFoundException.java
@@ -0,0 +1,4 @@
+package com.grongo.cloud_storage_app.exceptions.storageExceptions;
+
+public class ItemNotFoundException {
+}
---
+++ src/main/java/com/grongo/cloud_storage_app/services/items/impl/StorageServiceImpl.java
@@ -0,0 +1,4 @@
+package com.grongo.cloud_storage_app.services.items.impl;
+
+public class StorageServiceImpl {
+}
